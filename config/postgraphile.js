5e774289db1fcd9c1aeb096e79c21aec81b07908
Régis BRUNET BLEC <regis.blec@gmail.com>
2022-12-13 08:50:06 +0100

    export schema to json and graphql

--- config/postgraphile.js
+++ config/postgraphile.js
@@ -3,19 +3,25 @@ const { postgraphile } = require('postgraphile')
 const { DATABASE, PG_USER, PASSWORD, HOST, PG_PORT, PG_URL } = process.env
 
 module.exports = postgraphile(
-   /* {
-        database: DATABASE,
-        user: PG_USER,
-        password: PASSWORD,
-        host: HOST,
-        port: PG_PORT,
+
+  /* 
+  {
+    database: DATABASE,
+    user: PG_USER,
+    password: PASSWORD,
+    host: HOST,
+    port: PG_PORT,
   },  
-   */ 
-    PG_URL,
-    'public',
-    {
-        watchPg: true,
-        graphiql: true,
-        enhanceGraphiql: true, // permet d'avoir l'explorer
-    }
+  DATABASE_URL || "postgres://user:pass@host:5432/dbname",
+  */ 
+  PG_URL,
+  'public',
+  {
+    watchPg: true,
+    graphiql: true,
+    // graphiql: false, //! en mode production
+    enhanceGraphiql: true, //* permet d'avoir l'explorer
+    exportJsonSchemaPath: ('./graphql/schema.json'),
+    exportGqlSchemaPath: ('./graphql/schema2.graphql'),
+  }
 );
